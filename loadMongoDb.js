e23e5769a6e46ba533bd5adebea67b1540928b3b
James Rowe <jamesrowe08@gmail.com>
2016-07-20 21:04:31 -0500

    Adding db load script

---
+++ loadMongoDb.js
@@ -0,0 +1,5 @@
+db.productPriceCollection.insert({"productId" : 15117729, "amount" : 200.99, "currencyCode" : "USD" });
+db.productPriceCollection.insert({"productId" : 16483589, "amount" : 20.5, "currencyCode" : "USD" });
+db.productPriceCollection.insert({"productId" : 16696652, "amount" : 20.5, "currencyCode" : "USD" });
+db.productPriceCollection.insert({"productId" : 16752456, "amount" : 20.5, "currencyCode" : "USD" });
+db.productPriceCollection.insert({"productId" : 15643793, "amount" : 20.5, "currencyCode" : "USD" });
